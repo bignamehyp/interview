73c3a3e94c96994afdbc4236888456c8c08b6ee4
huangyp <huangyp@cs.uw.edu>
2015-03-06 23:34:00 -0800

    LintCode Commit

--- python/leetcode/BalancedBST.py
+++ python/leetcode/BalancedBST.py
@@ -1,15 +1,14 @@
-class BlancedBST:
-    def isBalanced(self, root):
-        if root is None:
-            return True
-        else:
-            if self.isBalanced(root.left) and self.isBalanced(root.right):
-                return abs(self.depth(root.left) - self.depth(root.right)) <= 1
-            else:
-                return False
+def isBalanced(self, root):
+    # write your code here
+    height = self.height(root)
+    return height >= 0        
 
-    def depth(self, root):
-        if root is None:
-            return -1
-        else:
-            return max(self.depth(root.left), self.depth(root.right)) + 1
+def height(self, root):
+    if root == None:
+        return 0
+    lh = self.height(root.left)
+    rh = self.height(root.right)
+    if lh >= 0 and rh >= 0 and abs(lh - rh) <= 1:
+            return max(lh, rh) +  1
+    else:
+        return -1
